46aeed29def415affc55795b423420d21480eaea
Henrique Silveira <henriquemartinsilveira2@gmail.com>
2023-09-27 02:05:46 -0300

    first commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ButtonContainer } from './styles'
+
+const Button = ({label, onClick }) => {
+    return (
+        <ButtonContainer onClick={onClick}>
+            {label}
+        </ButtonContainer>
+    )
+}
+
+export default Button
